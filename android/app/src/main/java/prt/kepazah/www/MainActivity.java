7dc3a55566d6673c6a4fe7b818fe5da1236d3e10
kepazah <ikaritensai@gmail.com>
2022-06-30 19:36:38 -0400

    APK

---
+++ android/app/src/main/java/prt/kepazah/www/MainActivity.java
@@ -0,0 +1,5 @@
+package prt.kepazah.www;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'prt.kepazah.www',
+  appName: 'PRT online',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
