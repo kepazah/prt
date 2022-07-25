5e10016dd94b7b535cd8819eefce104832500385
kepazah <ikaritensai@gmail.com>
2022-07-25 18:18:48 -0400

    API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+
+
+  ngOnInit() {
+
+    this.obtenerFeriados();
+    
+  }
+
+
+  obtenerFeriados() {
+
+    fetch('https://apis.digital.gob.cl/fl/feriados/2022').then(dato=>{
+      if (dato.ok){
+        dato.json().then(listaFeriados=>{
+      console.log(listaFeriados);
+        })
+      }
+    })
+  }
+
+
+  
 }
